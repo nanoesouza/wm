bfff49cd0b250cac882ec99f34dede4182b030e0
Adriano Elias <adriano.elias@crmall.com>
2022-06-01 17:22:10 -0300

    add polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+DIR="$HOME/.config/polybar"
+HDMI=$(xrandr --query | grep 'HDMI1')
+
+# Terminate already running bar instances
+killall -q polybar
+
+echo "---" | tee -a /tmp/polybar.log
+polybar -q main -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+
+if [[ $HDMI = *connected* ]]; then
+	polybar -q bar2 -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+fi
